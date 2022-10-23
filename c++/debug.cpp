c67d061b55cd07f9cafca55a32a07963194cfa99
Edward Guilfoyle <s4576111@fac-login-1.local>
2022-10-23 11:06:15 +1000

    Updated naming conventions for driver and debug builds to better reflect use cases. Updated Makefile to reflect changes.

---
+++ c++/debug.cpp
@@ -0,0 +1,96 @@
+#include "matrix.h"
+#include "losses.h"
+#include "layers.h"
+#include "metrics.h"
+#include "optimizers.h"
+
+using namespace matrix;
+
+int main() {
+    // Lets setup our data
+    std::vector<std::vector<float>> in { 
+	                       {1.0, 2.0, 3.0},
+			       {-4.0, -5.0, -6.0},  
+			       {7.0, 8.0, 9.0} };
+    
+    std::vector<std::vector<float>> true_in {{0, 1},
+					   {1, 0},
+					   {0, 1}};
+    Matrix<float> X(3, 3);
+    X.set_matrix(in);
+    Matrix<float> y_true(3, 2);
+    y_true.set_matrix(true_in);
+
+    Dense layer1(3, 2);
+    ReLU layer2;
+    Dense layer3(2, 2);
+    SoftmaxCrossEntropy layer4;
+    print(X);
+
+    Matrix<float> out1 = layer1.forward(X);
+    print(out1);
+    Matrix<float> out2 = layer2.forward(out1);
+    print(out2);
+    Matrix<float> out3 = layer3.forward(out2);
+    print(out3);
+    std::cout << "Start of softmax \n";
+    Matrix<float> out4 = layer4.forward(out3, y_true);
+    print(out4);
+    std::cout << layer4.get_loss() << std::endl;
+    std::cout << metric::accuracy(y_true, out4) << "\n";
+
+    std::cout << "Start of backprop relu and dense" << std::endl;
+
+    Matrix<float> test1 = layer2.backward(out1, out2);
+    print(test1);
+    Matrix<float> test2 = layer1.backward(X, test1);
+    print(test2);
+    print(layer1.get_dbiases());
+    print(layer1.get_dweights());
+
+    std::cout << "START OF TEST" << std::endl;
+
+    std::vector<std::vector<float>> test_in {{0.7, 0.1, 0.2},
+					   {0.1, 0.5, 0.4},
+					   {0.02, 0.9, 0.08}};
+    Matrix<float> softmaxOut(3, 3);
+    softmaxOut.set_matrix(test_in);
+    Matrix<float> test5 = layer4.backward(softmaxOut, y_true);
+    print(test5);
+    
+    std::cout << "Start of backprop full" << std::endl;
+    Matrix<float> back4 = layer4.backward(out4, y_true);
+    print(back4);
+    Matrix<float> back3 = layer3.backward(out2, back4);
+    print(back3);
+    Matrix<float> back2 = layer2.backward(out1, back3);
+    print(back2);
+    Matrix<float> back1 = layer1.backward(X, back2);
+    print(back1);
+    
+    std::cout << "Start Of optimizer test" << std::endl;
+    std::cout << "      Weights pre: " << std::endl;
+    print(layer3.get_weights());
+    print(layer3.get_dweights());
+    print(layer1.get_weights());
+    print(layer1.get_dweights());
+    std::cout << "      Biases pre: " << std::endl;
+    print(layer3.get_biases());
+    print(layer3.get_dbiases());
+    print(layer1.get_biases());
+    print(layer1.get_dbiases());
+    optimizer::SGD sgd(1, 0);
+    sgd.pre_update();
+    sgd.update(&layer3);
+    sgd.update(&layer1);
+    sgd.post_update();
+    std::cout << "      Weights post: " << std::endl;
+    print(layer3.get_weights());
+    print(layer1.get_weights());
+    std::cout << "      Biases post: " << std::endl;
+    print(layer3.get_biases());
+    print(layer1.get_biases());
+    return 1;
+}
+
+
--- c++/driver.cpp
+++ c++/driver.cpp
@@ -3,94 +3,164 @@
 #include "layers.h"
 #include "metrics.h"
 #include "optimizers.h"
+#include "data/data.h"
 
 using namespace matrix;
 
-int main() {
-    // Lets setup our data
-    std::vector<std::vector<float>> in { 
-	                       {1.0, 2.0, 3.0},
-			       {-4.0, -5.0, -6.0},  
-			       {7.0, 8.0, 9.0} };
-    
-    std::vector<std::vector<float>> true_in {{0, 1},
-					   {1, 0},
-					   {0, 1}};
-    Matrix<float> X(3, 3);
-    X.set_matrix(in);
-    Matrix<float> y_true(3, 2);
-    y_true.set_matrix(true_in);
+void handle_input(Matrix<double>& x_train, Matrix<double>& y_train, 
+		Matrix<double>& x_test, Matrix<double>& y_test, int N);
 
-    Dense layer1(3, 2);
+/* main()
+* -----
+* The main entry point for this program. Creates and runs 
+* an Artificial Neural Network. Architecture constists of: 
+* 			- Dense(2, 16)
+* 			- ReLU()
+* 			- Dense(16, 2)
+* 			- SoftMax()
+* 
+* A single command line argument can be parsed to control the
+* size of the input data to the neural network. Increments
+* of 100 from 100-1000. 
+*
+* @argc: The length of argv
+* @argv: Inputs from the command line
+*
+* Returns: 1 if the program failed, 0 if successful.
+*/
+int main(int argc, char *argv[]) {
+	// Some code to handle command line input
+	int N = 100;
+	if (argc > 1) {
+		N = std::stoi(argv[1]);
+		if (N > 1000 || N < 100) {
+			N = 1000;
+		}
+	}
+
+	std::cout << "N: " << N << std::endl;
+	int train_size = N * 0.8;
+
+	// Define the training and testing Matrixes
+    Matrix<double> x_train(train_size, 2);
+    Matrix<double> y_train(train_size, 2);
+    Matrix<double> x_test(N - train_size, 2);
+    Matrix<double> y_test(N - train_size, 2);
+	// This function allocates the data to these matrices
+	handle_input(x_train, y_train, x_test, y_test, N);
+
+	// Construct our network
+    Dense layer1(2, 16);
     ReLU layer2;
-    Dense layer3(2, 2);
+    Dense layer3(16, 2);
     SoftmaxCrossEntropy layer4;
-    print(X);
+    optimizer::SGD sgd(1.0, 0.001);
 
-    Matrix<float> out1 = layer1.forward(X);
-    print(out1);
-    Matrix<float> out2 = layer2.forward(out1);
-    print(out2);
-    Matrix<float> out3 = layer3.forward(out2);
-    print(out3);
-    std::cout << "Start of softmax \n";
-    Matrix<float> out4 = layer4.forward(out3, y_true);
-    print(out4);
-    std::cout << layer4.get_loss() << std::endl;
-    std::cout << metric::accuracy(y_true, out4) << "\n";
+	// Main algorithimic loop
+    for (int i = 0; i < 2001; i++) {
 
-    std::cout << "Start of backprop relu and dense" << std::endl;
+		Matrix<double> out1 = layer1.forward(x_train);
+		Matrix<double> out2 = layer2.forward(out1);
+		Matrix<double> out3 = layer3.forward(out2);
+		Matrix<double> out4 = layer4.forward(out3, y_train);
+		double loss = layer4.get_loss();
+		double acc = metric::accuracy(y_train, out4);
 
-    Matrix<float> test1 = layer2.backward(out1, out2);
-    print(test1);
-    Matrix<float> test2 = layer1.backward(X, test1);
-    print(test2);
-    print(layer1.get_dbiases());
-    print(layer1.get_dweights());
+		Matrix<double> back4 = layer4.backward(out4, y_train);
+		Matrix<double> back3 = layer3.backward(out2, back4);
+		Matrix<double> back2 = layer2.backward(out1, back3);
+		Matrix<double> back1 = layer1.backward(x_train, back2);
+		
+		
+		sgd.pre_update();
+		sgd.update(&layer3);
+		sgd.update(&layer1);
+		sgd.post_update();
+		if (i % 100 == 0) {
+			// Let's test the network every 100 iterations
+			Matrix<double> outtest1 = layer1.forward(x_test);
+			Matrix<double> outtest2 = layer2.forward(outtest1);
+			Matrix<double> outtest3 = layer3.forward(outtest2);
+			Matrix<double> outtest4 = layer4.forward(outtest3, y_test);
+			double losstest = layer4.get_loss();
+			double acctest = metric::accuracy(y_test, outtest4);
 
-    std::cout << "START OF TEST" << std::endl;
+			std::cout << "epoch: " << i;
+			std::cout << ", acc: " << acc;
+			std::cout << ", loss: " << loss;
+			std::cout << ", acc_test: " << acctest;
+			std::cout << ", loss_test: " << losstest;
+			std::cout << ", lr: " << std::fixed << sgd.get_lr() << std::endl;
+		}
 
-    std::vector<std::vector<float>> test_in {{0.7, 0.1, 0.2},
-					   {0.1, 0.5, 0.4},
-					   {0.02, 0.9, 0.08}};
-    Matrix<float> softmaxOut(3, 3);
-    softmaxOut.set_matrix(test_in);
-    Matrix<float> test5 = layer4.backward(softmaxOut, y_true);
-    print(test5);
-    
-    std::cout << "Start of backprop full" << std::endl;
-    Matrix<float> back4 = layer4.backward(out4, y_true);
-    print(back4);
-    Matrix<float> back3 = layer3.backward(out2, back4);
-    print(back3);
-    Matrix<float> back2 = layer2.backward(out1, back3);
-    print(back2);
-    Matrix<float> back1 = layer1.backward(X, back2);
-    print(back1);
-    
-    std::cout << "Start Of optimizer test" << std::endl;
-    std::cout << "      Weights pre: " << std::endl;
-    print(layer3.get_weights());
-    print(layer3.get_dweights());
-    print(layer1.get_weights());
-    print(layer1.get_dweights());
-    std::cout << "      Biases pre: " << std::endl;
-    print(layer3.get_biases());
-    print(layer3.get_dbiases());
-    print(layer1.get_biases());
-    print(layer1.get_dbiases());
-    optimizer::SGD sgd(1, 0);
-    sgd.pre_update();
-    sgd.update(&layer3);
-    sgd.update(&layer1);
-    sgd.post_update();
-    std::cout << "      Weights post: " << std::endl;
-    print(layer3.get_weights());
-    print(layer1.get_weights());
-    std::cout << "      Biases post: " << std::endl;
-    print(layer3.get_biases());
-    print(layer1.get_biases());
-    return 1;
+    }
+
+
+    return 0;
 }
 
-
+/* handle_input()
+* -----
+* Selects which data should be used in training the neural network.
+* Selected conditionally on the size of N.
+*
+* @x_train: The training data samples
+* @y_train: The training data labels
+* @x_test: The test data samples
+* @y_test: The test data labels
+* @N: The amount of data that should be inputted into the Matrix's
+*/
+void handle_input(Matrix<double>& x_train, Matrix<double>& y_train, 
+		Matrix<double>& x_test, Matrix<double>& y_test, int N) {
+	if (N == 100) {
+		x_train.set_matrix(x_train_raw_100);
+		y_train.set_matrix(y_train_raw_100);
+		x_test.set_matrix(x_test_raw_100);
+		y_test.set_matrix(y_test_raw_100);
+	} else if (N == 200) {
+		x_train.set_matrix(x_train_raw_200);
+		y_train.set_matrix(y_train_raw_200);
+		x_test.set_matrix(x_test_raw_200);
+		y_test.set_matrix(y_test_raw_200);
+	} else if (N == 300) {
+		x_train.set_matrix(x_train_raw_300);
+		y_train.set_matrix(y_train_raw_300);
+		x_test.set_matrix(x_test_raw_300);
+		y_test.set_matrix(y_test_raw_300);
+	} else if (N == 400) {
+		x_train.set_matrix(x_train_raw_400);
+		y_train.set_matrix(y_train_raw_400);
+		x_test.set_matrix(x_test_raw_400);
+		y_test.set_matrix(y_test_raw_400);
+	} else if (N == 500) {
+		x_train.set_matrix(x_train_raw_500);
+		y_train.set_matrix(y_train_raw_500);
+		x_test.set_matrix(x_test_raw_500);
+		y_test.set_matrix(y_test_raw_500);
+	} else if (N == 600) {
+		x_train.set_matrix(x_train_raw_600);
+		y_train.set_matrix(y_train_raw_600);
+		x_test.set_matrix(x_test_raw_600);
+		y_test.set_matrix(y_test_raw_600);
+	} else if (N == 700) {
+		x_train.set_matrix(x_train_raw_700);
+		y_train.set_matrix(y_train_raw_700);
+		x_test.set_matrix(x_test_raw_700);
+		y_test.set_matrix(y_test_raw_700);
+	} else if (N == 800) {
+		x_train.set_matrix(x_train_raw_800);
+		y_train.set_matrix(y_train_raw_800);
+		x_test.set_matrix(x_test_raw_800);
+		y_test.set_matrix(y_test_raw_800);
+	} else if (N == 900) {
+		x_train.set_matrix(x_train_raw_900);
+		y_train.set_matrix(y_train_raw_900);
+		x_test.set_matrix(x_test_raw_900);
+		y_test.set_matrix(y_test_raw_900);
+	} else if (N == 1000) {
+		x_train.set_matrix(x_train_raw_1000);
+		y_train.set_matrix(y_train_raw_1000);
+		x_test.set_matrix(x_test_raw_1000);
+		y_test.set_matrix(y_test_raw_1000);
+	}
+}
